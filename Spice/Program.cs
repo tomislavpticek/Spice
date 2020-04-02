02835fa91604cbbe3893209675660eb24325117b
Tomislav Ptiček <tomislav.pticek@outlook.com>
2020-04-02 22:18:14 +0200

    Complete category

--- Spice/Program.cs
+++ Spice/Program.cs
@@ -20,7 +20,7 @@ namespace Spice
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls(new[] { "http://0.0.0.0:5001" });
                 });
     }
 }
